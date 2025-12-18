f1e141fda202ce7e0a1051c6b31de693eb0ea8fa
RS <shah.reena26@gmail.com>
2025-12-18 16:16:24 +0530

    feat: initial today view and layout

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//Show today's date
+const todayDateE1 =document.getElementById("todayDate");
+const today = new Date();
+const options = { weekday:"long", day:"numeric", month:"short"};
+todayDateE1.textContent = today.toLocaleDateString("en-In",options);
+
 
 const habitInput = document.getElementById('habitInput');
 const addHabitBtn = document.getElementById('addHabitBtn');
